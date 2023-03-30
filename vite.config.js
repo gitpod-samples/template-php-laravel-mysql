8aaec55562b08bb28947fba0dbafa0011179d9bb
Joao Patricio <ijpatricio@gmail.com>
2023-03-30 21:47:06 +0100

    Updates to lateste Laravel 10 with Sail Environment

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import {defineConfig} from 'vite'
+import laravel from 'laravel-vite-plugin'
+
+require('dotenv').config()
+
+const extendedViteDevServerOptions = {}
+
+if (process.env.GITPOD_VITE_URL) {
+    extendedViteDevServerOptions.hmr = {
+        protocol: 'wss',
+        host: new URL(process.env.GITPOD_VITE_URL).hostname,
+        clientPort: 443
+    }
+}
+
+export default defineConfig({
+    server: {
+        ...extendedViteDevServerOptions
+    },
+    plugins: [
+        laravel({
+            input: 'resources/js/app.js',
+            refresh: true,
+        }),
+    ],
+})
